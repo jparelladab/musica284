e1184b8aaccf9632a3e8d0680fd4ca60a89f9f97
joan parellada <joan.parellada789@gmail.com>
2020-05-30 11:42:02 +0200

    index show and new working for composer piece and level

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,10 @@ Rails.application.routes.draw do
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   devise_for :users
+  devise_scope :user do
+   get '/users/sign_out' => 'devise/sessions#destroy'
+  end
+
   root to: 'pages#home'
   get 'about', to: 'pages#about'
   get 'contact', to: 'pages#contact'
