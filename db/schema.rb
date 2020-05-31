f2eb40cea3d4952ba7015197482f466a53317bb0
joan parellada <joan.parellada789@gmail.com>
2020-05-31 14:37:04 +0200

    user follow logic and some frontend

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_05_30_161424) do
+ActiveRecord::Schema.define(version: 2020_05_31_113311) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -40,6 +40,13 @@ ActiveRecord::Schema.define(version: 2020_05_30_161424) do
     t.string "name"
     t.string "biography"
     t.integer "rating"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "follows", force: :cascade do |t|
+    t.integer "follower_id"
+    t.integer "followed_user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
