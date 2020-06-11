64e61408b71bcfad5db5ec05cb8456da0cc65e87
joan parellada <joan.parellada789@gmail.com>
2020-06-11 12:01:37 +0200

    Videos displaying correctly

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,4 +1,5 @@
 module ApplicationHelper
+
   def find_photo(element)
     if element.photo.attached?
         return element.photo.key
@@ -6,4 +7,11 @@ module ApplicationHelper
         return "nqeWdH9m2xAE9MhopqXgeZFJ.jpg"
     end
   end
+
+  def youtube_video(url)
+    video_id = url.split("?v=")[1]
+    embed_url = "https://www.youtube.com/embed/" + video_id
+    render :partial => 'shared/video', :locals => { :url => embed_url }
+  end
+
 end
