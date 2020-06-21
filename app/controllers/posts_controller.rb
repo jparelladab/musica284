9cec8631cac7daf35cf7e74c0272cd2164f8b21f
joan parellada <joan.parellada789@gmail.com>
2020-06-21 16:28:32 +0200

    posts logic index create and destroy working on profile and general wall

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,24 +2,27 @@ class PostsController < ApplicationController
   #before_action :set_post, only: [:show, :edit, :update, :destroy]
 
   def index
-    @user_posts = params[:user_id].nil? ? nil : User.find(params[:user_id]).posts
+    @user_posts = params[:user_id].nil? ? nil : User.find(params[:user_id]).posts.sort_by {|post| post.created_at}.reverse!
     @my_posts = current_user.posts.sort_by {|post| post.created_at}.reverse!
     @all_posts = @my_posts + (current_user.followings.map {|foll| foll.posts}.flatten).sort_by {|post| post.created_at}.reverse!
+    #@general_wall = params[:general_wall]
   end
 
   def show
   end
 
   def new
+    #@general_wall = params[:general_wall]
   end
 
   def create
     @post = Post.new(user_id: params[:user_id], text: params[:post][:text])
     respond_to do |format|
+    #@general_wall = params[:general_wall]
       if @post.save
-    @my_posts = current_user.posts.sort_by {|post| post.created_at}.reverse!
-    @user_posts = params[:user_id].nil? ? nil : User.find(params[:user_id]).posts
-    @all_posts = @my_posts + (current_user.followings.map {|foll| foll.posts}.flatten).sort_by {|post| post.created_at}.reverse!
+        @my_posts = current_user.posts.sort_by {|post| post.created_at}.reverse!
+        @user_posts = params[:user_id].nil? ? nil : User.find(params[:user_id]).posts.sort_by {|post| post.created_at}.reverse!
+        @all_posts = @my_posts + (current_user.followings.map {|foll| foll.posts}.flatten).sort_by {|post| post.created_at}.reverse!
         format.js
       else
         format.js
@@ -35,9 +38,20 @@ class PostsController < ApplicationController
   end
 
   def destroy
+    @post = Post.find(params[:id])
+    @usr = current_user
+    #@general_wall = params[:general_wall]
+    respond_to do |format|
+      if @post.destroy
+        @my_posts = current_user.posts.sort_by {|post| post.created_at}.reverse!
+        @all_posts = @my_posts + (current_user.followings.map {|foll| foll.posts}.flatten).sort_by {|post| post.created_at}.reverse!
+        format.js
+      else
+        format.js
+      end
+    end
   end
 
   private
-
 
 end
