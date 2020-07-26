ba8c71cc3b252123e0bc12d22e8b1169dd87dbdd
joan parellada <joan.parellada789@gmail.com>
2020-07-26 18:42:31 +0200

    added Add Video partial in user profile

--- app/controllers/videos_controller.rb
+++ app/controllers/videos_controller.rb
@@ -1,7 +1,7 @@
 class VideosController < ApplicationController
   def index
     if params[:user_id].present?
-      @user_videos = User.find(params[:user_id]).videos
+      @user_videos = User.find(params[:user_id]).videos.sort_by {|v| v.created_at}.reverse!
     else
       @all_videos = Video.all
     end
@@ -13,6 +13,18 @@ class VideosController < ApplicationController
   def new
     respond_to do |format|
       format.js
+    end
+  end
+
+  def create
+    @video = Video.new(user_id: params[:user_id], url: params[:video][:url], title: params[:video][:title])
+    puts params[:video]
+    if @video.save
+      flash[:notice] = "video successfully created"
+      redirect_to user_path(@video.user)
+    else
+      flash[:notice] = "Sorry, an error has occurred."
+      render partial: "new"
     end
   end
 
